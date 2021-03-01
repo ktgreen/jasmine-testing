a5cc2fd9f8154b06c034e3bdc4e935b71ec02231
ktgreen <kateegreen.95@gmail.com>
2021-03-01 20:38:00 +0000

    change code

---
+++ spec/calcSpec.js
@@ -0,0 +1,7 @@
+describe("Calculator", function() {
+    describe("Addition Tests", function() {
+        it("should return 42", function() {
+            expect(addition(20,42)).toBe(42)
+        })
+    })
+})
